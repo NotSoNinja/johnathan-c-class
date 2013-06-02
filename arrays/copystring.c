f84936baae8ee48a0456a5d7bf2564c348ecaec8
Johnathan Adams <jwaohnathan@live.com>
2013-06-02 13:34:38 -0700

    now it actually does stuff

--- arrays/copystring.c
+++ arrays/copystring.c
@@ -1,9 +1,12 @@
 //implement a function that does the same thing as strcopy
 
+#include <stdio.h>
+
 int main(){
 	char str1[] = "Happy Birthday";
 	char str2[] = "Panda";
 	totallyNotStrcopy(str1, str2);
+	printf("%s"str1);
 	return 0;
 }
 
