9ec327ce2398553c68ff9c13b9b45a5f6d86e632
Johnathan Adams <jwaohnathan@live.com>
2013-06-02 13:46:35 -0700

    you fixed it so it fishes for invalid cases CORRECTLY

--- arrays/copystring.c
+++ arrays/copystring.c
@@ -5,7 +5,7 @@
 int main(){
 	char str1[] = "Happy Birthday";
 	char str2[] = "Panda";
-	printf("1 for error: %d", totallyNotStrcopy(str1, str2));
+	printf("1 for error: %d\n", totallyNotStrcopy(str1, str2));
 	printf("%s\n", str1);
 	return 0;
 }
@@ -15,15 +15,18 @@ int totallyNotStrcopy(char a[], char b[]){
 	int aLength;
 	int bLength;
 	char carrot = ' '; // char-at
+	// gets the length of a
 	while(carrot != '\0'){
 		carrot = a[index];
 		index++;
 	}
 	aLength = index + 1;
 	index = 0;
+	carrot = ' ';
 	
+	//gets the length of b
 	while(carrot != '\0'){
-		carrot = a[index];
+		carrot = b[index];
 		index++;
 	}
 	bLength = index + 1;
