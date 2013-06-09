52c9d974becb066a487f6b237928c2a55889d261
Johnathan Adams <jwaohnathan@live.com>
2013-06-09 12:08:33 -0700

    saving before running

---
+++ pointers/stringmagic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	
+	char a[] = "This is a test string";
+	int strpos = 0;
+	int i;
+	
+	for(i = 0; i < strlen(a); i++){
+		
+		printf("%s\n", a+i)
+		
+	}
+	return 0;
+}
