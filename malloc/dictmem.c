8f0f586a636cdf70142adb273fb4938ee8cb3dba
Johnathan Adams <jwaohnathan@live.com>
2013-06-23 13:38:13 -0700

    I think it both functions and is decently efficient

--- malloc/dictmem.c
+++ malloc/dictmem.c
@@ -8,11 +8,10 @@ int main(){
 	/* copied from hangmanA3, reads in dictionary and counts words */
 	char word[80];
 	int i;
-	int index = 0;
-	int count = 0;
 	char * dict; 
 	char *errorCheck;
 	char * * ptrs;
+	char *tempword;
 	
 	FILE * dictionary = fopen("/usr/share/dict/words", "r");
 	if(!dictionary){
@@ -27,24 +26,23 @@ int main(){
 	}while(errorCheck);
 	rewind(dictionary);
 	
-	//new, I think it might work?
+	//new, works.
 	dict = malloc(80 * numberOfWords * sizeof(char));
-	ptrs = malloc(numberOfWords + sizeof(char *));
+	ptrs = malloc(numberOfWords * sizeof(char *));
 	if(NULL == dict || NULL == ptrs){
 		printf("Null Pointer Exception\n");
 		return 2;
 	}
-	do{
+	for (i = 0; i < numberOfWords; i++) {
 		errorCheck = fgets(word, sizeof(word), dictionary);
 		word[strlen(word) - 1] = '\0';
-		strcpy((dict + index), word);
-		ptrs[count] = &(dict + index);
-		count++;
-		index += 80;
-	}while(errorCheck);
-	index = 0;
-	// causes bus error
-	
+		tempword = malloc(strlen(word) * sizeof(char));
+		strcpy(tempword, word);
+		*(ptrs+i) = tempword;
+	}
+	for (i = 0; i < numberOfWords; i++) {
+		printf("%s\n", *(ptrs+i));
+	}
 	free(dict);
 	return 0;
 }
