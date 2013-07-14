c1c6505c20869d0d9d126c8e9cb4ebb5cce37610
Matt Harris <mharris@splunk.com>
2013-07-14 13:36:14 -0700

    Defined pop

--- calculator/stack+queue/stacknqueue.c
+++ calculator/stack+queue/stacknqueue.c
@@ -18,6 +18,28 @@ Stack *makestack(int size){
 int deletestack(Stack *);
 int arraysize(Stack *);
 
+//Return the first element in the array and remove it from the array.
+void *pop(Stack *stack) {
+	int i;
+	void *ret;
+
+	ret = stack->array[0];  //Save the head element so we don't lose it.
+
+	//Move all the next elements up one;
+	for (i = 1; i < stack->used; i++) {
+		stack->array[i-1] = stack->array[i];
+	}
+	//Clear the end element
+	stack->array[i] = 0;
+
+	//Decrement the stack size;
+	stack->used--;
+
+	//Done!
+	return ret;
+}
+
+
 // 
 // typedef struct st {
 // 	void *array[];
