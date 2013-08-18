f847bfb070dfbbf24d0be34b12d650d615a8479a
Johnathan Adams <johnathan@Moe.(none)>
2013-08-18 15:31:08 -0700

    maybe it'll commit now

--- djikstra/djikstra.h
+++ djikstra/djikstra.h
@@ -1,9 +1,13 @@
+#ifndef _djikstra_h_
+#define _djikstra_h_
 #include <stdio.h>
 #include <stdlib.h>
-#include "djikstra.c"
+// #include "djikstra.c"
 
 int traverse(int [7][7], int, int, int, int, int *);
 int arrcmp(int *, int, int);
 int printarray(int *, int, char *);
 int selectleast(int *, int);
 int traverse2(int [7][7], int, int, int *, int, int, int);
+
+#endif
